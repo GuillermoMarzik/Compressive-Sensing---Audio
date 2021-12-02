6d9703792f43733696fb4a9c378dfab2fb2c0eb4
Guillermo <marzikg@gmail.com>
2021-12-01 21:11:13 -0300

    Rec básica

--- Reconstruccion_basica/pre_processing_owlqn.py
+++ Reconstruccion_basica/pre_processing_owlqn.py
@@ -9,14 +9,15 @@ Created on Fri Jul 30 20:20:46 2021
 import numpy as np
 import mdct
 from scipy.fft import dct, idct
-import soundfile as sf
 from pylbfgs import owlqn
-import pickle
 from scipy.stats import bernoulli,poisson
 from stdct import sdct,isdct
 import math 
+from FHT import FHT_FFT
+from stdht import sdht,isdht
+import pywt
 
-def pre_pros_pad(signal,frame_size,window='boxcar',transform='stdct'):
+def pre_pros_pad(signal,transform='stdct'):
     """
     Esta función toma una señal y la concatena con un vector de ceros para darle el formato requerido por el callback del método OWL-QN
 
@@ -24,10 +25,6 @@ def pre_pros_pad(signal,frame_size,window='boxcar',transform='stdct'):
     ----------
     signal : array
         Señal a la que se le van a agregar ceros.
-    frame_size : int
-        Largo del frame de la transformada tiempo-frecuencia. 
-    window : string, opcional
-        Tipo de ventana utilizada en los distintos frames. El default es 'boxcar', ventana rectangular.
     transform : string, opcional
         Transformada tiempo-frecuencia elegida como base de representación. El default es 'stdct'.
 
@@ -37,16 +34,21 @@ def pre_pros_pad(signal,frame_size,window='boxcar',transform='stdct'):
         Señal formateada para el método OWL-QN.
 
     """
-    frame_step = frame_size.copy()
-    if ((transform == 'stdct') or (transform == 'stdht')):
-        signal_t = sdct(signal,frame_size,frame_step,window=window)
-    elif (transform=='mdct'):
+    dim2 = None
+    if ((transform == 'stdct') or (transform == 'stdht') or (transform=='mdct')):
         signal_t = mdct.fast.mdct(signal)
+        N_aux = signal_t.shape[0]*signal_t.shape[1]
+        dim2 = signal_t.shape[1]
+        tot_size = N_aux-512
     else:
-        signal_t = np.zeros(math.ceil(math.log(len(signal),2)))
-    tot_size = signal_t.size
+        #wavelet
+        signal_t = math.ceil(math.log(len(signal),2))
+        tot_size = 2**signal_t
     signal_pad = np.pad(signal,(0,np.int(tot_size-len(signal))))
-    return signal_pad    
+    if transform == 'sdct' or transform == 'stht':
+        signal_t = sdct(signal_pad,2048,2048,window='boxcar')
+        dim2 = signal_t.shape[1]
+    return signal_pad,dim2
 
 
 def subsamp(s, signal, lambda_, med_mat = 'Gauss'):
@@ -104,11 +106,11 @@ def subsamp(s, signal, lambda_, med_mat = 'Gauss'):
     b = signal.T.flat[ri].astype(float)
     return ri,b   
 
-def rec_owlqn(ri,b,c,N,progreso=False):
+def rec_owlqn(ri,b,c,N,progreso=False,transformada='dct',wav = None,dim2=None):
     """
     Reconstrucción de una señal mediante Compressive Sensing utilizando el algoritmo OWL-QN.
-    Basado y adaptado del código de XXX
-    REFERENCIA
+    Basado y adaptado del código de Robert Taylor
+    http://www.pyrunner.com/weblog/2016/05/26/compressed-sensing-python/
 
     Parameters
     ----------
@@ -131,46 +133,89 @@ def rec_owlqn(ri,b,c,N,progreso=False):
     """
 
     def evaluate(x, g, step):
-        """An in-memory evaluation callback."""
+        if transformada == 'dct':
+            x2 = x.reshape((N)).T
+            Ax2 = idct(x2, norm='ortho')
+        elif transformada == 'sdct':
+            x2 = x.reshape((2048,dim2))
+            Ax2 = isdct(x2,frame_step=2048,frame_length=2048,window='boxcar')
+        elif transformada == 'stht':
+            x2 = x.reshape((2048,dim2))
+            Ax2 = isdht(x2,frame_step=2048,frame_length=2048,window='boxcar')       
+        elif transformada == 'dht':
+            x2 = x.reshape((N)).T
+            Ax2 = FHT_FFT(x2)
+        elif transformada == 'mdct':
+            x2 = x.reshape((512,dim2))
+            Ax2 = mdct.fast.imdct(x2)
+        elif transformada == 'dwt':
+            x2 = x.reshape((N)).T
+            x3 = pywt.array_to_coeffs(x2,coff_shape,output_format='wavedec')
+            Ax2 = pywt.waverec(x3, wav,mode='periodization')
 
-        # we want to return two things: 
-            # (1) the norm squared of the residuals, sum((Ax-b).^2), and
-            # (2) the gradient 2*A'(Ax-b)
-
-        # expand x columns-first
-        x2 = x.reshape((N)).T
-
-        # Ax is just the inverse 2D dct of x2
-        Ax2 = idct(x2, norm='ortho')    
-
-        # stack columns and extract samples
         Ax = Ax2.T.flat[ri].reshape(b.shape)
 
-        # calculate the residual Ax-b and its 2-norm squared
         Axb = Ax - b
         fx = np.sum(Axb ** 2)
 
+        if transformada == 'dct' or transformada == 'sdct' or transformada == 'sdht' or transformada == 'dht' or transformada == 'dwt':
+            Axb2 = np.zeros(Ax2.shape)
+            Axb2[ri] = Axb
+        elif transformada == 'mdct':
+            Axb2 = np.zeros((x.shape)[0]-512)
+            Axb2.T.flat[ri] = Axb
+            
+        if transformada == 'dct':
+            AtAxb2 = 2 * dct(Axb2,norm='ortho')
+        elif transformada == 'sdct':
+            AtAxb2 = 2 * sdct(Axb2,2048,2048,window='boxcar')
+        elif transformada == 'stht':
+            AtAxb2 = 2 * sdht(Axb2,2048,2048,window='boxcar')      
+        elif transformada == 'dht':
+            AtAxb2 = 2 * FHT_FFT(Axb2)
+        elif transformada == 'mdct':
+            AtAxb2 = 2 * mdct.fast.mdct(Axb2)
+        elif transformada == 'dwt':
+            AtAxb2 = pywt.wavedec(Axb2,wav,mode='periodization')
+            AtAxb2 = 2 * pywt.coeffs_to_array(AtAxb2)[0]
 
-        # project residual vector (k x 1) onto blank image (ny x nx)
-        Axb2 = np.zeros(Ax2.shape)
-        Axb2[ri] = Axb
-
-        # A'(Ax-b) is just the 2D dct of Axb2
-        AtAxb2 = 2 * dct(Axb2,norm='ortho')
         AtAxb = AtAxb2.reshape(x.shape)
 
-        # copy over the gradient vector
         np.copyto(g, AtAxb)
 
         return fx
     def progress(x, g, fx, xnorm, gnorm, step, k, ls):
         return 0
+    if transformada == 'dwt':
+        aux = np.zeros(N)
+        aux_w = pywt.wavedec(aux,wav,mode='periodization')
+        global coff_shape
+        _,coff_shape= pywt.coeffs_to_array(aux_w)
+        del aux, aux_w
     if progreso:
         Xat2 = owlqn(N, evaluate, None, c)
     else:
         Xat2 = owlqn(N, evaluate, progress, c)
-    Xat = Xat2.reshape(N).T
-    Xa = idct(Xat,norm='ortho')
+
+    if transformada == 'sdct':
+        Xat = Xat2.reshape((2048,dim2))
+        Xa = isdct(Xat,frame_step=2048,frame_length=2048,window='boxcar')
+    if transformada == 'sdht':
+        Xat = Xat2.reshape((2048,dim2))
+        Xa = isdht(Xat,frame_step=2048,frame_length=2048,window='boxcar')        
+    elif transformada == 'mdct':
+        Xat = Xat2.reshape((512,dim2))
+        Xa = mdct.fast.imdct(Xat)
+    elif transformada == 'dwt':
+        Xat2 = Xat2.ravel()
+        Xat = pywt.array_to_coeffs(Xat2,coff_shape,output_format='wavedec')
+        Xa = pywt.waverec(Xat,wav,mode='periodization')
+    elif transformada == 'dct':    
+        Xat = Xat2.reshape(N).T
+        Xa = idct(Xat,norm='ortho')
+    elif transformada == 'dht':
+        Xat = Xat2.reshape(N).T
+        Xa = FHT_FFT(Xat)       
     return Xa
 
 
--- Reconstruccion_basica/rec_signal.py
+++ Reconstruccion_basica/rec_signal.py
@@ -7,7 +7,7 @@ Created on Tue Oct 26 20:05:17 2021
 """
 import numpy as np
 import soundfile as sf
-from pre_processing_owlqn import subsamp,rec_owlqn
+from pre_processing_owlqn import subsamp,rec_owlqn,pre_pros_pad
 
 #A signal sampled at Nyquist rate is loaded.
 Xorig,fs = sf.read('tono.wav')
@@ -15,13 +15,20 @@ Xorig,fs = sf.read('tono.wav')
 sample_sizes = (0.05, 0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95)
 lambda_ = np.array([0.051,0.105,0.17,0.228,0.285,0.36,0.425,0.52,0.6,0.71,0.8,0.91,1.05,1.25,1.35,1.7,1.95,2.4,2.9])
 
+#Padding with zeros (for MDCT, STDCT, STDHT and DWT)
+Xorig,dim2 = pre_pros_pad(Xorig,transform='sdct')
+
 #Random samples of the signal are retrieved, following a given probability distribution.
 ri,b = subsamp(sample_sizes[10],Xorig,lambda_[10],med_mat = 'Gauss')
 
-N = len(Xorig)
+transform = 'sdct'
+if transform == 'mdct':
+    N = len(Xorig) +512
+else:
+    N = len(Xorig)
 c = 0.0001 #orthanwise constant for OWL-QN algorithm, sparsity control
 #Signal reconstruction from a given percentage of the total number of samples
-Xa = rec_owlqn(ri,b,c,N,progreso=False)
+Xa = rec_owlqn(ri,b,c,N,progreso=True, transformada='dwt',wav='db4',dim2=dim2)
 
 
 
